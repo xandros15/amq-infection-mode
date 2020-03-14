58a23399dcd2d06002481664a1b2d936cf4f3a21
xandros15 <xandros92@gmail.com>
2020-03-14 17:57:43 +0100

    add description for UserScript

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // @name         AMQ Infected Mode
 // @namespace    amq-infected-mode
 // @version      1
-// @description
+// @description  script allow to play in custom mode in anime music quiz
 // @author       xandros and Supersj1997
 // @match        https://animemusicquiz.com/*
 // @grant        none
