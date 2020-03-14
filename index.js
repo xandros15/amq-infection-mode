ceb90119882405fdde7537c4dc37748d12a9e151
xandros15 <xandros92@gmail.com>
2020-03-14 17:56:29 +0100

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         AMQ Infected Mode
+// @namespace    amq-infected-mode
+// @version      1
+// @description
+// @author       xandros and Supersj1997
+// @match        https://animemusicquiz.com/*
+// @grant        none
+// ==/UserScript==
